e954d174ce15d6b9f4c7c697e3c5e1f33becf442
Lakshmi <mindyalavenkatadurgalakshmi@gmail.com>
2025-08-13 14:24:38 +0530

    MY first experience with React

---
+++ My_app/app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+
+// Add the code below
+return <div>
+<h1>Hello Kalvium</h1>
+      <h2>My first react experience</h2>
+</div>
+}
+export default App;
